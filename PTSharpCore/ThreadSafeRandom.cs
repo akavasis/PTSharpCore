e0d6168db3be4f0763b11d85e8d3c573d924341f
akavasis <55655604+akavasis@users.noreply.github.com>
2020-10-24 11:30:30 +0300

    Thread safe random number generation

---
+++ PTSharpCore/ThreadSafeRandom.cs
@@ -0,0 +1,62 @@
+using System;
+
+namespace PTSharpCore
+{
+    /// ThreadSafeRandom.cs - Thread safe random number generation
+    /// Code expanded from article by Stephen Toub
+    /// https://devblogs.microsoft.com/pfxteam/getting-random-numbers-in-a-thread-safe-way/ 
+    class ThreadSafeRandom
+    {
+        private static Random _global = new Random();
+        [ThreadStatic]
+        private static Random _local;
+
+        public static int Next()
+        {
+            Random inst = _local;
+            if (inst is null)
+            {
+                int seed;
+                lock (_global) seed = _global.Next();
+                _local = inst = new Random(seed);
+            }
+            return inst.Next();
+        }
+
+        public static int Next(int i)
+        {
+            Random inst = _local;
+            if (inst is null)
+            {
+                int seed;
+                lock (_global) seed = _global.Next();
+                _local = inst = new Random(seed);
+            }
+            return inst.Next(i);
+        }
+
+        public static double NextDouble()
+        {
+            Random inst = _local;
+            if (inst is null)
+            {
+                int seed;
+                lock (_global) seed = _global.Next();
+                _local = inst = new Random(seed);
+            }
+            return inst.NextDouble();
+        }
+
+        public static double NextDouble(Random rand)
+        {
+            Random inst = rand;
+            if (inst is null)
+            {
+                int seed;
+                lock (_global) seed = _global.Next();
+                _local = inst = new Random(seed);
+            }
+            return inst.NextDouble();
+        }
+    }
+}
