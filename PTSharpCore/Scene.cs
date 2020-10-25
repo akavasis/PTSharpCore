d3550938b6d603882d71a20db6a58105711e7b61
akavasis <55655604+akavasis@users.noreply.github.com>
2020-10-25 11:58:09 +0200

    Switch to array structure

--- PTSharpCore/Scene.cs
+++ PTSharpCore/Scene.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace PTSharpCore
@@ -7,15 +8,29 @@ namespace PTSharpCore
         internal Colour Color = new Colour();
         internal ITexture Texture = null;
         internal double TextureAngle = 0;
-        private List<IShape> shapeList = new List<IShape>();
-        private List<IShape> lightList = new List<IShape>();
-        internal IShape[] Lights = new IShape[] { };
-        private IShape[] Shapes = new IShape[] { };
         private Tree tree;
         internal int rays = 0;
 
-        public Scene() { }
-            
+        internal IShape[] Shapes; 
+        internal IShape[] Lights;
+
+        public Scene() 
+        {
+            Shapes = new IShape[0];
+            Lights = new IShape[0];        
+        }
+
+        internal void Add(IShape p)
+        {
+            Array.Resize(ref Shapes, Shapes.GetLength(0) + 1);
+            Shapes[Shapes.GetLength(0) - 1] = p;
+            if (p.MaterialAt(new Vector()).Emittance > 0)
+            {
+                Array.Resize(ref Lights, Lights.GetLength(0) + 1);
+                Lights[Lights.GetLength(0) - 1] = p;
+            }
+        }
+
         public void Compile()
         {
             foreach(IShape shape in Shapes)
@@ -26,17 +41,6 @@ namespace PTSharpCore
             {
                 tree = Tree.NewTree(Shapes);
             }
-        }
-
-        internal void Add(IShape shape)
-        {
-            shapeList.Add(shape);
-            if(shape.MaterialAt(new Vector()).Emittance > 0)
-            {
-                lightList.Add(shape);
-                Lights = lightList.ToArray();
-            }
-            Shapes = shapeList.ToArray();
         }
 
         int RayCount()
